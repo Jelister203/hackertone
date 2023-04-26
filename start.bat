37b6e2ea2d979d8c97273db4bc610548c727ec59
Jelister203 <93206051+Jelister203@users.noreply.github.com>
2023-04-26 15:34:00 +0300

    v1.0.1

--- start.bat
+++ start.bat
@@ -1,9 +1,9 @@
 cd data
 python -m venv venv
-pip install -r requirments.txt
+pip install -r requirements.txt
 cd ../parser
 python -m venv venv
-pip install -r requirments.txt
+pip install -r requirements.txt
 cd ..
 START /b python data\manage.py runserver
 START /b python parser\main.py
